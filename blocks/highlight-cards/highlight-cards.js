0cd8cc2a3079269a18d81824af697584be754199
S-g14 <shraddha.gupta4@tcs.com>
2026-09-16 18:53:21 +0530

    Updated code

--- blocks/highlight-cards/highlight-cards.js
+++ blocks/highlight-cards/highlight-cards.js
@@ -1,20 +1,22 @@
 import { createOptimizedPicture } from '../../scripts/aem.js';
 import { moveInstrumentation } from '../../scripts/scripts.js';
 
+const FIELD_CLASSES = [
+  'highlight-cards-card-image',
+  'highlight-cards-card-category',
+  'highlight-cards-card-body',
+];
+
 export default function decorate(block) {
   const ul = document.createElement('ul');
   [...block.children].forEach((row) => {
     const li = document.createElement('li');
     moveInstrumentation(row, li);
     while (row.firstElementChild) li.append(row.firstElementChild);
-    [...li.children].forEach((div) => {
-      if (div.children.length === 1 && div.querySelector('picture')) {
-        div.className = 'highlight-cards-card-image';
-      } else if (!div.querySelector('*')) {
-        div.className = 'highlight-cards-card-category';
-      } else {
-        div.className = 'highlight-cards-card-body';
-      }
+    // classify each field div by its fixed position (image, category, text),
+    // not by its content, so empty/unset fields stay visible and editable
+    [...li.children].forEach((div, i) => {
+      div.className = FIELD_CLASSES[i] || 'highlight-cards-card-body';
     });
     ul.append(li);
   });
