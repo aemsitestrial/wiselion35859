2941fa114e8c4746b24f8bfd6d246e823aee97bd
S-g14 <shraddha.gupta4@tcs.com>
2026-09-17 11:10:30 +0530

    Refactor highlight-cards.js for class assignment logic

--- blocks/highlight-cards/highlight-cards.js
+++ blocks/highlight-cards/highlight-cards.js
@@ -1,25 +1,18 @@
 import { createOptimizedPicture } from '../../scripts/aem.js';
 import { moveInstrumentation } from '../../scripts/scripts.js';
 
-const FIELD_CLASSES = [
-  'highlight-cards-card-image',
-  'highlight-cards-card-category',
-  'highlight-cards-card-body',
-];
-
 export default function decorate(block) {
   const ul = document.createElement('ul');
   [...block.children].forEach((row) => {
     const li = document.createElement('li');
     moveInstrumentation(row, li);
     while (row.firstElementChild) li.append(row.firstElementChild);
-    // classify each field div by its fixed position (image, category, text),
-    // not by its content, so empty/unset fields stay visible and editable
-    [...li.children].forEach((div, i) => {
-      div.className = FIELD_CLASSES[i] || 'highlight-cards-card-body';
+  [...li.children].forEach((div) => {
+      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'highlight-cards-card-body';
+      else div.className = 'cards-card-body';
     });
     ul.append(li);
-  });
+  });     
   ul.querySelectorAll('picture > img').forEach((img) => {
     const optimizedPic = createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }]);
     moveInstrumentation(img, optimizedPic.querySelector('img'));
