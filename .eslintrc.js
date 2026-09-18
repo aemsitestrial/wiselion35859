4e26e046f0b608b79945d3cf57a56d4d3738b873
S-g14 <shraddha.gupta4@tcs.com>
2026-09-18 14:32:16 +0530

    Add xwalk/max-cells rule to ESLint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,5 +18,7 @@ module.exports = {
     'import/extensions': ['error', { js: 'always' }], // require js file extensions in imports
     'linebreak-style': ['error', 'unix'], // enforce unix linebreaks
     'no-param-reassign': [2, { props: false }], // allow modifying properties of param
+    // the highlight card renders one cell per field plus one per overlay toggle
+    'xwalk/max-cells': ['error', { 'highlight-card': 9, '*': 4 }],
   },
 };
