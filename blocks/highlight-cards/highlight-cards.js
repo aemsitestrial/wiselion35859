55c9fe7deb3280205f1a49859f3ce6f414e429a4
S-g14 <shraddha.gupta4@tcs.com>
2026-09-21 16:19:57 +0530

    updated

--- blocks/highlight-cards/highlight-cards.js
+++ blocks/highlight-cards/highlight-cards.js
@@ -10,6 +10,9 @@ const FIELD_CLASSES = [
 ];
 
 export default function decorate(block) {
+  block.classList.add('highlight-cards');
+  [...block.children].forEach((row) => row.classList.add('highlight-cards-item'));
+
   const ul = document.createElement('ul');
   [...block.children].forEach((row) => {
     const li = document.createElement('li');
