0d0831074f12318a8bc0b91273b6a78e44a602b2
S-g14 <shraddha.gupta4@tcs.com>
2026-09-21 12:57:27 +0530

    Update xwalk/max-cells ESLint rule configuration
    Refactor xwalk/max-cells rule to use max and exceptions properties.

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,6 +19,10 @@ module.exports = {
     'linebreak-style': ['error', 'unix'], // enforce unix linebreaks
     'no-param-reassign': [2, { props: false }], // allow modifying properties of param
     // the highlight card renders one cell per field plus one per overlay toggle
-    'xwalk/max-cells': ['error', { 'highlight-card': 9,'highlighted-card': 9, '*': 4 }],
-  },
-};
+    'xwalk/max-cells': ['error', {
+  max: 4,
+  exceptions: {
+    'highlight-card': 9,
+    'highlighted-card': 9,
+  }
+}],
