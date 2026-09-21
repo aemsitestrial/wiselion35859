84cc028cb871052a9294c9f00680cb9a21570bff
S-g14 <shraddha.gupta4@tcs.com>
2026-09-21 15:02:34 +0530

    Remove 'xwalk/max-cells' rule from ESLint
    Removed custom rule 'xwalk/max-cells' from ESLint configuration.

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,10 +18,5 @@ module.exports = {
     'import/extensions': ['error', { js: 'always' }], // require js file extensions in imports
     'linebreak-style': ['error', 'unix'], // enforce unix linebreaks
     'no-param-reassign': [2, { props: false }], // allow modifying properties of param
-    'xwalk/max-cells': ['error', {
-      '*': 4,
-      'highlighted-card': 7,
-      'highlight-card': 7,
-    }],
   },
 };
